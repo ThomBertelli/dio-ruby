f0e7b30d16e6c7f22b6d65839c9d823ce8f9b0cd
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2023-12-18 21:22:30 -0300

    add desafio_1

---
+++ desafio_1/nome_completo_idade.rb
@@ -0,0 +1,11 @@
+
+puts "Qual é seu nome ?"
+nome = gets.chomp
+
+puts "Qual é seu sobrenome ?"
+sobrenome = gets.chomp
+
+puts "Qual é sua idade  ?"
+idade = gets.chomp
+
+puts "Olá! #{nome} #{sobrenome} de #{idade} anos."
